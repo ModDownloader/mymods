b676283abdd3775d410316ce3654c5de35adf7ca
miaueczek@gmail.com <unconfigured@null.spigotmc.org>
2020-12-03 10:54:33 +0100

    added potion

---
+++ src/main/java/pl/janekkoduje/resourcegenerator/potion/PotionkaPotion.java
@@ -0,0 +1,107 @@
+
+package pl.janekkoduje.resourcegenerator.potion;
+
+import pl.janekkoduje.resourcegenerator.procedures.PotionkaPotionStartedappliedProcedure;
+import pl.janekkoduje.resourcegenerator.ResourceGeneratorModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effect;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.LivingEntity;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@ResourceGeneratorModElements.ModElement.Tag
+public class PotionkaPotion extends ResourceGeneratorModElements.ModElement {
+	@ObjectHolder("resource_generator:potionka")
+	public static final Effect potion = null;
+	@ObjectHolder("resource_generator:potionka")
+	public static final Potion potionType = null;
+	public PotionkaPotion(ResourceGeneratorModElements instance) {
+		super(instance, 72);
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent
+	public void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+
+	@SubscribeEvent
+	public void registerPotion(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		public PotionCustom() {
+			super(new EffectInstance(potion, 3600));
+			setRegistryName("potionka");
+		}
+	}
+
+	public static class EffectCustom extends Effect {
+		private final ResourceLocation potionIcon;
+		public EffectCustom() {
+			super(EffectType.BENEFICIAL, -205);
+			setRegistryName("potionka");
+			potionIcon = new ResourceLocation("resource_generator:textures/piotionka.png");
+		}
+
+		@Override
+		public String getName() {
+			return "effect.potionka";
+		}
+
+		@Override
+		public boolean isBeneficial() {
+			return true;
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRender(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public void applyAttributesModifiersToEntity(LivingEntity entity, AbstractAttributeMap attributeMapIn, int amplifier) {
+			World world = entity.world;
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				PotionkaPotionStartedappliedProcedure.executeProcedure($_dependencies);
+			}
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
